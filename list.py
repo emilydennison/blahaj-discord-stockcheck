cff0a6e6254965a77fb3d11e5892849f94d2068b
emily <hello@emilyd.me>
2022-11-30 23:29:12 +0000

    First commit

--- list.py
+++ list.py
@@ -1,6 +1,9 @@
 import json
 import colorama
 import requests
+from discord import webhook
+from discord import embeds
+import datetime
 
 with open("stores.json") as f:
     # Parsed from (search for "allStores=["):
@@ -16,7 +19,12 @@ def calculate_stock(availability):
         return None
 
     store_name = store_name[0]
-
+    if list(availability["buyingOption"]["cashCarry"].keys())[0] == "range":
+        return {
+            "store": store_name,
+            "quantity": 0,
+            "next_restock": None,
+        }
     availability_info = availability["buyingOption"]["cashCarry"]["availability"]
     quantity = int(availability_info["quantity"])
 
@@ -66,25 +74,70 @@ def pretty_print_stock(stock):
 
 def main():
     colorama.init()
+    for maybeShonk  in range(1, 3):
+        if maybeShonk == 1:
+            shorkType = "30373588"
+            shorkName = "Bighåj"
+        elif maybeShonk == 2:
+            shorkType = "20540663"
+            shorkName = "Smolhåj"
+        print("Fetching " + shorkName + " listings…\n")
 
-    print("Fetching BLÅHAJ listings…\n")
+        big_availabilities = requests.get("https://api.ingka.ikea.com/cia/availabilities/ru/gb?itemNos=" + shorkType + "&expand=StoresList,Restocks,SalesLocations", headers={
+            "Accept": "application/json;version=2",
+            "X-Client-ID": "b6c117e5-ae61-4ef5-b4cc-e0b1e37f0631"
+        })
+        big_availabilities = big_availabilities.json()
+        sharks = []
+        for availability in big_availabilities["availabilities"]:
+            stock = calculate_stock(availability)
+            if stock is None:
+                continue
+            sharks.append(stock)
+        sharks.sort(key=lambda s: s["quantity"], reverse=True)
 
-    availabilities = requests.get("https://api.ingka.ikea.com/cia/availabilities/ru/gb?itemNos=30373588&expand=StoresList,Restocks,SalesLocations", headers={
-        "Accept": "application/json;version=2",
-        "X-Client-ID": "b6c117e5-ae61-4ef5-b4cc-e0b1e37f0631"
-    })
-    availabilities = availabilities.json()
+        for stock in sharks:
+            pretty_print_stock(stock)
+        send_webhook(sharks, shorkName)
 
-    sharks = []
-    for availability in availabilities["availabilities"]:
-        stock = calculate_stock(availability)
-        if stock is None:
-            continue
-        sharks.append(stock)
-    sharks.sort(key=lambda s: s["quantity"], reverse=True)
 
-    for stock in sharks:
-        pretty_print_stock(stock)
+def send_webhook(sharks, shorkName):
+    hook = webhook.SyncWebhook.from_url(
+        "#TODO INSERT YOUR OWN WEBHOOK HERE.")
+    messageEmbeds = [
+        embeds.Embed(
+            title="Blåhaj stock in the UK",\
+            description="Here is " + shorkName + " stock for today in the UK.",\
+            url="https://www.ikea.com/gb/en/p/blahaj-soft-toy-shark-30373588/",\
+            colour=0x6996AD,\
+            timestamp=datetime.datetime.today(),
+        )
+
+    ]
+    messageEmbeds[0].set_footer(text = "shonk")
+    messageEmbeds[0].set_thumbnail(url="https://www.ikea.com/gb/en/images/products/blahaj-soft-toy-shark__0877368_pe633607_s5.jpg")
+    messageEmbeds[0].set_author(name = "a sentient blåhaj")
+
+    # Break up the message into two chunks (embed char limit) 
+    sharks1 = sharks[:len(sharks)//2]
+    sharks2 = sharks[len(sharks)//2:]
+    rows1 = []
+    rows2 = []
+    for shark in sharks1:
+        if shark["next_restock"] == None:
+            rows1.append(shark["store"] + ": " + str(shark["quantity"]))
+        else:
+            rows1.append(shark["store"] + ": " + str(shark["quantity"] + ". Restock due: " + str(shark["next_restock"]["latestDate"])))
+    for shark in sharks2:
+        if shark["next_restock"] == None:
+            rows2.append(shark["store"] + ": " + str(shark["quantity"]))
+        else:
+            rows2.append(shark["store"] + ": " + str(shark["quantity"]) + ". Restock due: " + str(shark["next_restock"]["latestDate"]))
+    print(rows1)
+    messageEmbeds[0].add_field(name="Stock:", value="\n".join(rows1), inline=True)
+    messageEmbeds[0].add_field(name="Further stock:", value="\n".join(rows2), inline=True)
+
+    hook.send(content="", username="shark-alert-bot", embeds=messageEmbeds)
 
 
 main()
